ed5bf9cc84e0c092812fbcd09d9237a22306b40f
marvellous10 <nwachukwumarvelous90@gmail.com>
2024-05-01 13:43:42 +0100

    add allowed hosts

--- concordvote/settings.py
+++ concordvote/settings.py
@@ -17,9 +17,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 CORS_ALLOW_ALL_ORIGINS = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['concord-vote.onrender.com']
 
 
 # Application definition
